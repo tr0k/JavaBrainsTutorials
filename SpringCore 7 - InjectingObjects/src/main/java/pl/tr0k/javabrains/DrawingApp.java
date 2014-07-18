1b8e8c97a1f6307c39fde2c899895eedac8160e5
tr0k <trok92@gmail.com>
2014-07-18 15:56:09 +0200

    Initial commit with new module InjectingObjects

---
+++ SpringCore 7 - InjectingObjects/src/main/java/pl/tr0k/javabrains/DrawingApp.java
@@ -0,0 +1,17 @@
+package pl.tr0k.javabrains;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by tr0k on 2014-07-16.
+ */
+public class DrawingApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = context.getBean("triangle", Triangle.class);
+        triangle.draw();
+    }
+
+}

d3a9d8d81eea92a5d974ce4ac7de51618d1f25c9
tr0k <trok92@gmail.com>
2014-07-21 19:25:00 +0200

    InnerBeans module with aliases and idref

---
+++ SpringCore 8 - InnerBeans/src/main/java/pl/tr0k/javabrains/Point.java
@@ -0,0 +1,27 @@
+package pl.tr0k.javabrains;
+
+/**
+ * Created by tr0k on 2014-07-18.
+ */
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
